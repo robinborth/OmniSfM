26b7c2c96ce44c34a4dac4acf05542f7213dc4c2
Joshua Pusch <joshua.pusch@sap.com>
2024-06-26 00:22:05 +0200

    Push

--- scripts/process_folder.py
+++ scripts/process_folder.py
@@ -10,9 +10,12 @@ from subprocess import check_output
 
 register_heif_opener()
 
-def convert_to_png(in_path: str, out_path: str, pbar: tqdm):
+def convert_to_png(in_path: str, out_path: str, pbar: tqdm, file):
   img = Image.open(in_path)
   img.save(out_path)
+  exif_data = check_output(["./get_metadata/get_metadata", in_path])
+  fX, fY, cX, cY = get_intrinsics(exif_data)
+  file.write(f"{fX} {fY} {cX} {cY}")
   pbar.update(1)
 
 def write_info_for_file(path: str, file: str, pbar: tqdm):
@@ -53,20 +56,17 @@ def main():
   rgb_dir = os.path.join(out_data, "rgb")
   os.makedirs(rgb_dir, exist_ok=True)
 
-  # with tqdm(total=len(files), desc="Copy and convert images") as pbar:
-  #   threads = set()
-  #   # copy and convert images
-  #   for index, file in enumerate(files):
-  #     img_path = os.path.join(rgb_dir, f"{index:05d}.png")
-  #     thread = Thread(target=convert_to_png, args=(os.path.join(in_data, file), img_path, pbar))
-  #     threads.add(thread)
-  #     thread.start()
-  #   for thread in threads:
-  #     thread.join()
-
-  output = check_output(["./get_metadata/get_metadata", os.path.join(".", rgb_dir, files[0])])
-  print(files[1])
-
+  with open(os.path.join(rgb_dir, "intrinsics.txt"), "w") as f:
+    with tqdm(total=len(files), desc="Copy and convert images") as pbar:
+      threads = set()
+      # copy and convert images
+      for index, file in enumerate(files):
+        img_path = os.path.join(rgb_dir, f"{index:05d}.png")
+        thread = Thread(target=convert_to_png, args=(os.path.join(in_data, f), img_path, pbar, file))
+        threads.add(thread)
+        thread.start()
+      for thread in threads:
+        thread.join()
 
 def get_intrinsics(exif_data: str) -> Tuple[float, float, float, float]:
   j = json.loads(exif_data)
@@ -82,9 +82,9 @@ def get_intrinsics(exif_data: str) -> Tuple[float, float, float, float]:
   return (fX, fY, cX, cY)
 
 if __name__ == "__main__":
-  # main()
-  output = check_output(["./get_metadata/get_metadata", "/Users/I564278/Downloads/IMG_6479/IMG_6479.HEIC"])
-  fX, fY, cX, cY = get_intrinsics(output)
-  with open("lol.txt", "w") as file:
-    file.write("# id fX fY cX cY\n")
-    file.write(f"{1:05d} {fX} {fY} {cX} {cY}\n")
+  main()
+  # output = check_output(["./get_metadata/get_metadata", "/Users/I564278/Downloads/IMG_6479/IMG_6479.HEIC"])
+  # fX, fY, cX, cY = get_intrinsics(output)
+  # with open("lol.txt", "w") as file:
+  #   file.write("# id fX fY cX cY\n")
+  #   file.write(f"{1:05d} {fX} {fY} {cX} {cY}\n")
