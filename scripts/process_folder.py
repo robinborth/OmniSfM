2a4239d457599d51d263e15f9347041d5c6dd8db
Joshua Pusch <joshua.pusch@tum.de>
2024-06-26 00:33:59 +0200

    Improve

--- scripts/process_folder.py
+++ scripts/process_folder.py
@@ -10,28 +10,12 @@ from subprocess import check_output
 
 register_heif_opener()
 
-def convert_to_png(in_path: str, out_path: str, pbar: tqdm, file):
+def convert_to_png(in_path: str, out_path: str, pbar: tqdm, file, index: int):
   img = Image.open(in_path)
   img.save(out_path)
   exif_data = check_output(["./get_metadata/get_metadata", in_path])
   fX, fY, cX, cY = get_intrinsics(exif_data)
-  file.write(f"{fX} {fY} {cX} {cY}")
-  pbar.update(1)
-
-def write_info_for_file(path: str, file: str, pbar: tqdm):
-  output = check_output(["./get_metadata/get_metadata", os.path.join(path, file)])
-  # catch errors in ./get_metadata
-  try:
-      json.loads(output)
-  except json.decoder.JSONDecodeError:
-      print(os.path.join(path, file), output.decode("utf-8"))
-      return
-  filename = os.path.splitext(file)[0]
-  info_path = os.path.join(".", "data", os.path.basename(path), f"{filename}.txt")
-  os.makedirs(os.path.join(".", "data"), exist_ok=True)
-  os.makedirs(os.path.join(".", "data", os.path.basename(path)), exist_ok=True)
-  with open(info_path, "wb") as file:
-      file.write(output)
+  file.write(f"{index:05d} {fX} {fY} {cX} {cY}\n")
   pbar.update(1)
 
 def main():
@@ -56,23 +40,24 @@ def main():
   rgb_dir = os.path.join(out_data, "rgb")
   os.makedirs(rgb_dir, exist_ok=True)
 
-  with open(os.path.join(rgb_dir, "intrinsics.txt"), "w") as f:
+  with open(os.path.join(rgb_dir, "intrinsics.txt"), "w") as intrinsics_file:
+    intrinsics_file.write("# id fX fY cX cY\n")
     with tqdm(total=len(files), desc="Copy and convert images") as pbar:
       threads = set()
       # copy and convert images
       for index, file in enumerate(files):
         img_path = os.path.join(rgb_dir, f"{index:05d}.png")
-        thread = Thread(target=convert_to_png, args=(os.path.join(in_data, f), img_path, pbar, file))
+        thread = Thread(target=convert_to_png, args=(os.path.join(in_data, file), img_path, pbar, intrinsics_file, index))
         threads.add(thread)
         thread.start()
       for thread in threads:
         thread.join()
 
-def get_intrinsics(exif_data: str) -> Tuple[float, float, float, float]:
-  j = json.loads(exif_data)
-  focal_length = j["FocalLength"]
-  width = j["PixelXDimension"]
-  height = j["PixelYDimension"]
+def get_intrinsics(exif_str: str) -> Tuple[float, float, float, float]:
+  exif = json.loads(exif_str)
+  focal_length = exif["FocalLength"]
+  width = exif["PixelXDimension"]
+  height = exif["PixelYDimension"]
   pixel_size_x = 1 / width
   pixel_size_y = 1 / height
   fX = focal_length / pixel_size_x
@@ -83,8 +68,3 @@ def get_intrinsics(exif_data: str) -> Tuple[float, float, float, float]:
 
 if __name__ == "__main__":
   main()
-  # output = check_output(["./get_metadata/get_metadata", "/Users/I564278/Downloads/IMG_6479/IMG_6479.HEIC"])
-  # fX, fY, cX, cY = get_intrinsics(output)
-  # with open("lol.txt", "w") as file:
-  #   file.write("# id fX fY cX cY\n")
-  #   file.write(f"{1:05d} {fX} {fY} {cX} {cY}\n")
